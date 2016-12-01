066b23ef01061fbd3fff6615e72c60af0c1ba429
George Tony <gtony@suyati.com>
2016-12-01 12:08:27 +0530

    test addCar

--- lib/Parking.py
+++ lib/Parking.py
@@ -8,14 +8,20 @@ class Parking(object):
     def __len__(self):
         return len(self.lLot)
 
-    def slotFree(self):
+    def lotFree(self):
+        """
+        Check if there is lot free or not
+        """
         for index,value in enumerate(self.lLot):
             if not value:
                 return index
         return -1
 
     def addCar(self,sRegisration,sColor):
-        index = self.slotFree()
+        """
+        Add car to the lot if free lot is present
+        """
+        index = self.lotFree()
         if index != -1:
             self.lLot[index] = Lot(sRegisration,sColor)
             return "Allocated slot number: {}".format(index + 1)
--- tests/Class/Parking_test.py
+++ tests/Class/Parking_test.py
@@ -1,14 +1,53 @@
 import unittest
 from lib.Parking import Parking
+from lib.Lot import Lot
 class ParkingTest(unittest.TestCase):
     def test_init(self):
         """
         On creatation there should be empty Slots
+        1. assign non zero Parking lot
+        2. assign zero parking lot
+        3. assign negative number
         """
         oParking = Parking(1)
         self.assertEquals(len(oParking),1)
         for oLot in oParking.lLot:
             self.assertIsNone(oLot)
+        oParking = Parking(0)
+        self.assertEquals(len(oParking),0)
+        oParking = Parking(-1)
+        self.assertEquals(len(oParking),0)
+
+
+    def test_lotFree(self):
+        """
+        test lotFree method
+        1. Check lot free for non zero postive number
+        2. Check for zero
+        """
+        oParking = Parking(2)
+        self.assertEquals(oParking.lotFree(),0)
+        oParking.lLot[0] = 1
+        self.assertEquals(oParking.lotFree(),1)
+        oParking.lLot[1] = 1
+        self.assertEquals(oParking.lotFree(),-1)
+        oParking = Parking(0)
+        self.assertEquals(oParking.lotFree(),-1)
+
+    def test_addCar(self):
+        """
+        test addCar
+        1. Add non zero Cars. Check list filled up with Lot type
+        """
+        oParking = Parking(2)
+        sMessage = oParking.addCar("a","b")
+        self.assertEquals(sMessage,"Allocated slot number: {}".format(1))
+        self.assertIsInstance(oParking.lLot[0],Lot)
+        sMessage = oParking.addCar("a","b")
+        self.assertIsInstance(oParking.lLot[1],Lot)
+        self.assertEquals(sMessage,"Allocated slot number: {}".format(2))
+        sMessage = oParking.addCar("a","b")
+        self.assertEquals(sMessage,"Sorry, parking lot is full")
 
 
 if __name__ == '__main__':
